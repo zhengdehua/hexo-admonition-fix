0c0a375eb5cf5721f78e4c2893668961ec66de30
HowardCheng <howacheng@163.com>
2025-07-08 16:58:06 +0800

    fix list and quote render problems

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var md = require('markdown-it')({
-  html: true // enable HTML tags in source
-});
+var md = require('marked');
 
 hexo.extend.filter.register('before_post_render', function (data) {
   let strRegExp = '(^!!! *)(note|info|warning|error)(.*\n)((^ {2}.*\n|^\n)+)';
@@ -8,10 +6,10 @@ hexo.extend.filter.register('before_post_render', function (data) {
 
   let strData;
   if (admonitionRegExp.test(data.content)) {
-
     strData = data.content.replace(admonitionRegExp, function (matchStr, p1, p2, p3, p4) {
-
       let tableLineRegExp = new RegExp('^\\|(.*\\|)+$');
+      let listLineRegExp = new RegExp('^-.*');
+      let quoteLineRegExp = new RegExp('^>.*');
       p4 = p4.split(/\n|\r|\r\n/);
       let admonitionContent = '';
 
@@ -21,14 +19,24 @@ hexo.extend.filter.register('before_post_render', function (data) {
           continue;
         }
 
-        admonitionContent += v + '\n\n';
+        if (listLineRegExp.test(v.trim())) {
+          admonitionContent += '\n' + v + '<br>';
+          continue;
+        }
+
+        if (quoteLineRegExp.test(v.trim())) {
+          admonitionContent += v + '<br>';
+          continue;
+        }
+
+        admonitionContent += '\n' + v + '\n';
       }
 
       if (p3.replace(/\s+/g, '') === '""') {
-        return '<div class="admonition ' + p2.toLowerCase() + '">' + md.render(admonitionContent) + '</div>\n\n';
+        return '<div class="admonition ' + p2.toLowerCase() + '">' + md.parse(admonitionContent) + '</div>\n\n';
       } else {
         p3 = p3.trim() === '' ? p2 : p3.replace(/(^ |")|("| $)/g, '');
-        return '<div class="admonition ' + p2.toLowerCase() + '"><p class="admonition-title">' + p3 + '</p>' + md.render(admonitionContent) + '</div>\n\n';
+        return '<div class="admonition ' + p2.toLowerCase() + '"><p class="admonition-title">' + p3 + '</p>' + md.parse(admonitionContent) + '</div>\n\n';
       }
     });
 
