e3f3ad92cd7a8bf8bc7571c87427153b6f4dd6ef
HowardCheng <howacheng@163.com>
2025-08-10 00:17:44 +0800

    inject css

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # hexo-admonition-fix 使用指南
-本插件是对 hexo-admonition 的改写，具体如下：
 
+## 插件特性
+本插件是对 hexo-admonition 的改写，具体如下：
 - 对整体代码进行了重构；
 - 取消了必须要在空行之后开始；
 - 缩进改为了2个空格；
 - 修复了内容不换行问题；
 - 支持表格解析；
 - 很好地支持了数学公式；
-- 类型限制在note|info|warning|error四种。
+- 类型限制在 note|info|warning|error 四种。
 
+## 文件说明
+- `index-simple.js`：直接用 markedjs 解析块内容，有时候需要在 markdown 文件中用 `<br>` 换行；
+- `index.js`：对 `index-simple.js` 进行了优化，不需要在 markdown 文件中用 `<br>` 换行。
+
+## 参考资料
 其他使用指南参考 https://github.com/lxl80/hexo-admonition
 
 ## License
--- index-simple.js
+++ index-simple.js
@@ -83,10 +83,6 @@ hexo.extend.helper.register('hexo_admonition_css', function() {
 
 var marked = require('marked');
 
-var removeLastBr = function(line) {
-  return line.replace(/<br\/?>$/i, '');
-};
-
 var parseWithoutMath = function(content) {
   // 步骤1：提取公式并替换为占位符
   const mathPattern = /(\$\$.*?\$\$|\$.*?\$)/gs; // 匹配块级和行内公式
@@ -109,54 +105,11 @@ var parseWithoutMath = function(content) {
 
 hexo.extend.filter.register('before_post_render', function (data) {
   let admonitionRegExp = new RegExp('(^!!! *)(note|info|warning|error)(.*\n)((^ {2}.*\n|^\n)+)', 'gmi');
-  let lastBrRegExp = new RegExp(/(<br\/?>)+/i);
-  let tableLineRegExp = new RegExp('^\\|(.*\\|)+$');
-  let listLineRegExp = new RegExp('^-.*');
-  let quoteLineRegExp = new RegExp('^>.*');
 
   if (admonitionRegExp.test(data.content)) {
     data.content = data.content.replace(admonitionRegExp, function (matchedContent, p1, p2, p3, p4) {
-      p4 = p4.split(/\n|\r|\r\n/);
-      let content = '';
-      let renderedContent = '';
 
-      for (const v of p4) {
-        if (content == '' || content.endsWith('\n\n')
-          || content.endsWith('{% raw %}') || v.trim() == '{% endraw %}') {
-          content += v;
-          continue;
-        }
-
-        if (v.trim() == '') {
-          content = removeLastBr(content) + '\n\n';
-          continue;
-        }
-
-        if (lastBrRegExp.test(v.trim())) {
-          content = removeLastBr(content) + v + '\n';
-          continue;
-        }
-
-        if (tableLineRegExp.test(v.trim())) {
-          content += v.trim() + '\n';
-          continue;
-        }
-
-        if (listLineRegExp.test(v.trim())) {
-          content += '\n' + v + '<br>';
-          continue;
-        }
-
-        if (quoteLineRegExp.test(v.trim())) {
-          content = removeLastBr(content) + '\n' + v + '<br>';
-          continue;
-        }
-
-        content = removeLastBr(content) + '<br>' + v;
-      }
-
-      content = removeLastBr(content);
-      renderedContent = parseWithoutMath(content);
+      let renderedContent = parseWithoutMath(p4);
 
       if (p3.replace(/\s+/g, '') === '""') {
         return '<div class="admonition ' + p2.toLowerCase() + '">' + renderedContent + '</div>\n\n';
